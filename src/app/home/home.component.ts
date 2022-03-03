decf22f263c8c97aa7adae2b0ebb661edb538fbb
Max Kakurin <iveilwallet@yandex.ru>
2022-03-03 17:22:01 +0300

    https://github.com/iveil787/my-app.git

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+export class NzDemoButtonBasicComponent {}
